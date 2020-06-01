bdf8b8542411c2e8c62fd1f2697f191d9657968d
jessegan <jesse13579@gmail.com>
2020-06-01 16:05:05 -0400

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,44 @@ class Song
   attr_accessor :name, :artist_name
   @@all = []
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new_by_name(name)
+    song.save
+    song
+  end
+
+  def self.new_from_filename(filename)
+    song_components = filename.split(" - ")
+    artist = song_components[0].strip
+    name = song_components[1].strip
+    name.slice!(".mp3")
+    song = self.new_by_name(name)
+    song.artist_name = artist
+    song 
+  end
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
   def self.all
     @@all
   end
@@ -10,4 +48,22 @@ class Song
     self.class.all << self
   end
 
+  def self.find_by_name(name)
+    self.all.find {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    if self.find_by_name(name)
+      self.find_by_name(name)
+    else 
+      self.create_by_name(name)
+    end
+  end
+  
+  def self.alphabetical
+    self.all.sort_by {|song| song.name}
+  end
+
+
+  
 end
